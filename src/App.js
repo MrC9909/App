37898f2d8786c33b52745912fa34bdced00eaa16
R了个C <18701282528@163.com>
2019-07-27 10:12:15 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// 应用的根组件
+import React,{Component} from 'react';
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>App</div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//入口JS 
+import React from 'react';
+import ReactDOM,{render} from 'react-dom';
+import App from './App';
+
+render(<App></App>,document.querySelector("#root"));
+
+
